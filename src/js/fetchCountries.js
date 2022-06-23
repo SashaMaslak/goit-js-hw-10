5179dcef28455367da30365acb1616b19d517826
SashaMaslak <owmaslak@gmail.com>
2022-06-23 12:59:04 +0300

    fixed filter fields

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,7 @@
 const BASE_URL = 'https://restcountries.com/v3.1';
 
 function fetchCountries(name) {
-   return fetch(`${BASE_URL}/name/${name}`)
+   return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
       .then(response => {
          if (!response.ok) {
             return;
